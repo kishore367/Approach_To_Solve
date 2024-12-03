f18c7fef8b8ff07fe5fd02e50d2b831ea4a78b6d
kishore E <110755404+kishore367@users.noreply.github.com>
2024-12-03 19:49:58 +0530

    Time: 3 ms (72.34%), Space: 42.7 MB (83.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,38 @@
+/*Convert all uppercase letters to lowercase (or vice versa, depending on your preference).
+Remove all whitespace from the string.
+Remove or ignore special characters such as commas (,), colons (:), etc.
+Finally, compare the beginning and the end of the string to check if it reads the same forward and backward.*/
 class Solution {
     public boolean isPalindrome(String s) {
-        int start = 0, end = s.length()-1;
+        String str = s.toLowerCase();
 
-        while(start <= end){
-            char currStart = s.charAt(start);
-            char currEnd = s.charAt(end);
-            if(!Character.isLetterOrDigit(currStart)){
-                start++;
-            }else if(!Character.isLetterOrDigit(currEnd)){
-                end--;
-            }else{
-                if(Character.toLowerCase(currStart) != Character.toLowerCase(currEnd)){
-                    return false;
-                }
-                start++;
-                end--;
+        int first = 0;
+        int last = s.length()-1;
+
+        while(first<=last){
+            char f = str.charAt(first);
+            char l = str.charAt(last);
+
+            if(!Character.isLetterOrDigit(f)){
+                first++;
             }
+           else if(!Character.isLetterOrDigit(l)){
+                last--;
+            }
+
+            else{
+                if(f!= l)
+                return false;
+            
+
+            first++;
+            last--;
+
+        }
         }
 
         return true;
-
+       
 
     }
 }
